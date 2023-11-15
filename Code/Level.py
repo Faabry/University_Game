6eef044c1d53fc42d23417a265d9df657b618517
Airton Fabre <airtonfabre95@gmail.com>
2023-11-15 08:34:30 -0300

    adding files and diagram

---
+++ Code/Level.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Game import Game
+
+class Level(Game):
+    def __init__(self):
+        self.name = None
+        self.screen = None
+        self.entity_list = None
+
+    def level_up(self, ):
+        pass
+
---
+++ Code/Menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Game import Game
+
+class Menu(Game):
+    def __init__(self):
+        self.screen = None
+
+    def run(self, ):
+        pass
+
